16d98b4a5e1b4b531e260c59b26b63951073b709
Jens <65734789+jdrepo@users.noreply.github.com>
2025-11-02 19:06:24 +0100

    modify schedule

--- EntraJoined-SessionHostPrep.ps1
+++ EntraJoined-SessionHostPrep.ps1
@@ -6,7 +6,7 @@ $KerbRegPath = "HKLM:\SYSTEM\CurrentControlSet\Control\Lsa\Kerberos\Parameters"
 New-ItemProperty -Path $KerbRegPath -Name "CloudKerberosTicketRetrievalEnabled" -Value 1 -PropertyType DWord -Force
 
 # When you use Microsoft Entra ID with a roaming profile solution like FSLogix, 
-# the credential keys in Credential Manager must belong to the profile that’s currently loading. 
+# the credential keys in Credential Manager must belong to the profile thatâ€™s currently loading. 
 # This will let you load your profile on many different VMs instead of being limited to just one. 
 # To enable this setting, create the following registry value
 $LoadCredKeyRegPath = "HKLM:\Software\Policies\Microsoft\AzureADAccount"
@@ -17,4 +17,8 @@ New-ItemProperty -Path $LoadCredKeyRegPath -Name "LoadCredKeyFromProfile" -Value
 
 $time = [DateTime]::Now.AddMinutes(10)
 $hourMinute=$time.ToString("HH:mm")
-SchTasks.exe /Create /RU "SYSTEM"/SC ONCE /TN "Reboot once" /TR "shutdown.exe /r /t 120" /ST $hourMinute /F
+
+$User = "NT AUTHORITY\SYSTEM"
+$Trigger = New-ScheduledTaskTrigger -Once -At $hourMinute
+$Action = New-ScheduledTaskAction -Execute "c:\windows\system32\shutdown.exe" -Argument "-r -f -t 120"
+Register-ScheduledTask -TaskName "Reboot once" -Trigger $Trigger -User $User -Action $Action -RunLevel Highest -Force
